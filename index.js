8b3cdea25fe7fbec839575b097c85844c4841a2a
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-04 23:55:56 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  // Write your algorithm here  
+  for (let i = 0; i < array.length; i++) {
+		for (let x = i + 1; x < array.length; x++) {
+			if (array[i] + array[x] === target) {
+				return true;
+			}
+		}
+	}
+	return false;
 }
 
 /* 
   Write the Big O time complexity of your function here
+  Complexity -> O(n^2)
 */
 
 /* 
   Add your pseudocode here
+  1. Loop through the array the first time
+  2. Iterate so that each num comes to contact with the other at i + 1 times
+  3. If the sum of these two numbers add up the return true
+  4. Otherwise collapse the whole method
 */
 
 /*
   Add written explanation of your solution here
+  for i times up to the length of the array, iterate through the array items.
+  for each time looped over i add 1 to x
+  check the sum of x and i
+  if the sum matches the target return true
+  else return false
 */
 
 // You can run `node index.js` to view these console logs
